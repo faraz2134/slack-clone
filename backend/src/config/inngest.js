c6ccfe6f835bb465daa32e66cc9f1e39b3dc7565
faraz2134 <cse23201@glbitm.ac.in>
2025-08-27 17:19:55 +0530

    stream & sentry integrated

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import { User } from "../models/user.model.js"; // Import the User model
+import { upsertStreamUser } from "./stream.js"; // Import the Stream user function}
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "slack-clone" });
@@ -21,6 +22,11 @@ const syncUser = inngest.createFunction(
     };
 
     await User.create(newUser);
+    await upsertStreamUser({
+      id: newUser.clerkId.toString(),
+      name: newUser.name,
+      image: newUser.image,
+    })
   }
 );
 
@@ -31,6 +37,7 @@ const deleteUserFromDB = inngest.createFunction(
     await connectDB();
     const { id } = event.data;
     await User.deleteOne({ clerkId: id });
+    await deleteStreamUser(id.toString());
   }
 );
 
